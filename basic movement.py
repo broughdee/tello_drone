f09cff3fce46695fca2a07e9532a91ee380561b9
broughdee <105313001+broughdee@users.noreply.github.com>
2022-05-12 11:01:05 -0700

    Update basic movement.py

--- basic movement.py
+++ basic movement.py
@@ -3,12 +3,44 @@ from time import sleep
 
 t = tello.Tello()
 t.connect()
-
 print(t.get_battery())
 
+# take off + adjust to correct height (200cm)
 t.takeoff()
-t.flip_left()
-t.rotate_clockwise(360)
-t.flip_right()
-t.move_up()
+alt = t.get_height()
+
+while alt < 200:
+        t.move_up(20)
+
+while alt > 250:
+        t.move_down(20)
+
+# move + turn + move to necessary area
+sleep(.5)
+t.move_forward(600)
+sleep(.5)
+t.rotate_counter_clockwise(90)
+sleep(.5)
+t.move_forward(300)
+sleep(.5)
+
+#celebrate
+t.rotate_clockwise(540)
+t.move_up(50)
+t.move_down(75)
+t.move_up(15)
+t.rotate_clockwise(90)
+
+#return
+sleep(.5)
+t.move_forward(600)
+sleep(.5)
+t.rotate_counter_clockwise(90)
+sleep(.5)
+t.move_forward(300)
+sleep(.5)
+t.rotate_counter_clockwise(90)
+
+#land
 t.land()
+
